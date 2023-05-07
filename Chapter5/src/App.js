c16fd2aff35aebb5961a5490c4a3607f74547b96
rahulastrorock <100rahulsingh100@gmail.com>
2023-05-08 01:03:10 +0530

    Chapter5

---
+++ Chapter5/src/App.js
@@ -0,0 +1,22 @@
+// ## Namaste React Course by Akshay Saini
+// Chapter 05 - Let's Get Hooked
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+// AppLayout component to show: Header, Body, Footer
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
---
+++ Chapter5/src/components/Header.js
@@ -0,0 +1,21 @@
+import FoodFireLogo from "../../Images/Food Fire Logo.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={FoodFireLogo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
