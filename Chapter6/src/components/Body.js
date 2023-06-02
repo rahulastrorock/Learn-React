cdea456d79a5656e0d9b5bf447dfecb6af031298
rahulastrorock <100rahulsingh100@gmail.com>
2023-06-03 01:48:34 +0530

    chapter 6

---
+++ Chapter6/src/components/Body.js
@@ -0,0 +1,90 @@
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import shimmer from "./shimmer";
+
+const Body = () => {
+  // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredList, setfilteredList] = useState("");
+
+  //useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Fetch Data from API
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+
+      //optional chaining operator
+      setListOfRestraunt(json?.data?.cards[2]?.data?.data?.cards);
+      setfilteredList(json?.data?.cards[2]?.data?.data?.cards);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Conditional Rendering to show loading when data is not fetched
+  // if (listOfRestaurants.length === 0) {
+  //   return <h1>Loading...</h1>;
+  // }
+
+  // Conditional Rendering to show shimmer when data is not fetched
+  // if (listOfRestaurants.length === 0) {
+  //   return shimmer();
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    shimmer()
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="Search for Restaurants"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setfilteredList(filteredList);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setfilteredList(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredList.map((res) => (
+          <RestaurantCard key={res.data.id} resData={res} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ Chapter6/src/components/Header.js
@@ -0,0 +1,33 @@
+import FoodFireLogo from "../../Images/Food Fire Logo.png";
+import { useState } from "react";
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={FoodFireLogo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnName === "Login"
+                ? alert("You are logged in")
+                : alert("You are logged out");
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
