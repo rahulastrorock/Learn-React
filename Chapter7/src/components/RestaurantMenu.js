66b3937c61c4f2b1eb0871a5014a25c9d0c50143
rahulastrorock <100rahulsingh100@gmail.com>
2023-06-05 20:41:58 +0530

    chapter 7

---
+++ Chapter7/src/components/RestaurantMenu.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
+
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    try {
+      const data = await fetch(MENU_API + resId);
+      const json = await data.json();
+      setResInfo(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.data?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card;
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(",")} - {costForTwoMessage}
+      </p>
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li>
+            {item.card.info.name}-{item.card.info.price / 100}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
